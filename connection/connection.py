44dda9bc4701e6b8a15a6c1c709ba595376c810f
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-11-08 23:36:50 +0300

    Connection Successful NX9K

--- connection/connection.py
+++ connection/connection.py
@@ -1,19 +1,39 @@
 from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException, NetMikoAuthenticationException
 
-# Cihaz konfigürasyonu
 cisco_device = {
     'device_type': 'cisco_ios',
     'ip': '192.168.1.101',
     'username': 'admin',
     'password': 'Turkcell!2023',
-    'port': 22,  # SSH için 22, Telnet için 23
-    # 'secret': 'enable_password',  # Gerekirse enable şifresi
+    'port': 22,
 }
 
-# Cisco switch'e bağlan
+try:
+    # Cisco switch'e bağlan
+    net_connect = ConnectHandler(**cisco_device)
+    print("Bağlantı başarılı.")
+
+    # "show interfaces" komutunu gönder ve sonucunu al
+    output = net_connect.send_command('show interfaces')
+    print("show interfaces komutunun sonucu:\n")
+    print(output)
+
+    # Bağlantıyı kapat
+    net_connect.disconnect()
+
+except NetMikoTimeoutException:
+    print("Bağlantı zaman aşımına uğradı. Cihaz erişimi mümkün olmayabilir.")
+except NetMikoAuthenticationException:
+    print("Kimlik doğrulama hatası. Kullanıcı adı veya şifre hatalı olabilir.")
+except Exception as e:
+    print(f"Beklenmeyen bir hata oluştu: {str(e)}")
+
+# İkinci bağlantı yapılıyor, yukarıdaki bağlantının ardından eklenen kod bloğu
 net_connect = ConnectHandler(**cisco_device)
 print("Bağlantı başarılı.")
 
+# Diğer işlemler devam ediyor
 # Örnek 1: "show interfaces" komutunu gönder ve sonucunu al
 output = net_connect.send_command('show interface status')
 print("show interface status komutunun sonucu:\n")
@@ -37,9 +57,14 @@ config_commands = [
 ]
 output4 = net_connect.send_config_set(config_commands)
 print("\nKonfigürasyon değişikliği sonucu:\n")
-output5 = net_connect.send_command('show run interface ethernet 1/1 ')
+output5 = net_connect.send_command('show run interface ethernet 1/1')
 print(output4)
 print(output5)
+
+# Yedekleme komutunu gönder
+output = net_connect.send_command('write memory')
+print("Yedekleme komutunun sonucu:\n")
+print(output)
 
 # Bağlantıyı kapat
 net_connect.disconnect()

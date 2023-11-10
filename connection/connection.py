56608d22d275474fff7c7b6238ba056a96992654
MRIDVANC <24781973+MRIDVANC@users.noreply.github.com>
2023-11-10 15:16:49 +0300

    Connection Successful NX9K

--- connection/connection.py
+++ connection/connection.py
@@ -1,69 +1,76 @@
 from netmiko import ConnectHandler
 from netmiko import NetMikoTimeoutException, NetMikoAuthenticationException
-cisco_device = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.1.101',
-    'username': 'admin',
-    'password': 'Turkcell!2023',
-    'port': 22,
-}
 
-try:
-    # Cisco switch'e bağlan
-    net_connect = ConnectHandler(**cisco_device)
-    print("Bağlantı başarılı.")
+def connect_to_switch(ip_address):
+    switch_device = {
+        'device_type': 'cisco_ios',
+        'ip': ip_address,
+        'username': 'admin',
+        'password': 'Turkcell!2023',
+        'port': 22,
+    }
 
-    # "show interfaces" komutunu gönder ve sonucunu al
-    output = net_connect.send_command('show interfaces')
-    print("show interfaces komutunun sonucu:\n")
-    print(output)
+    try:
+        # Switch'e bağlan
+        net_connect = ConnectHandler(**switch_device)
+        print(f"Bağlantı başarılı. {ip_address} switch'ine bağlandınız.")
 
-    # Bağlantıyı kapat
-    net_connect.disconnect()
+        # "show interfaces" komutunu gönder ve sonucunu al
+        output = net_connect.send_command('show interfaces')
+        print("show interfaces komutunun sonucu:\n")
+        print(output)
 
-except NetMikoTimeoutException:
-    print("Bağlantı zaman aşımına uğradı. Cihaz erişimi mümkün olmayabilir.")
-except NetMikoAuthenticationException:
-    print("Kimlik doğrulama hatası. Kullanıcı adı veya şifre hatalı olabilir.")
-except Exception as e:
-    print(f"Beklenmeyen bir hata oluştu: {str(e)}")
+        # Diğer işlemler devam ediyor
+        # Örnek 1: "show interfaces" komutunu gönder ve sonucunu al
+        output = net_connect.send_command('show interface status')
+        print("show interface status komutunun sonucu:\n")
+        print(output)
 
-# İkinci bağlantı yapılıyor, yukarıdaki bağlantının ardından eklenen kod bloğu
-net_connect = ConnectHandler(**cisco_device)
-print("Bağlantı başarılı.")
+        # Örnek 2: "show ip route" komutunu gönder ve sonucunu al
+        output2 = net_connect.send_command('show lldp neighbors')
+        print("\nshow lldp komutunun sonucu:\n")
+        print(output2)
 
-# Diğer işlemler devam ediyor
-# Örnek 1: "show interfaces" komutunu gönder ve sonucunu al
-output = net_connect.send_command('show interface status')
-print("show interface status komutunun sonucu:\n")
-print(output)
+        output3 = net_connect.send_command('show mac address-table')
+        print("\nshow mac address komutunun sonucu:\n")
+        print(output3)
 
-# Örnek 2: "show ip route" komutunu gönder ve sonucunu al
-output2 = net_connect.send_command('show lldp neighbors')
-print("\nshow lldp komutunun sonucu:\n")
-print(output2)
+        # Örnek 3: Cihazın konfigürasyonunu değiştirme
+        config_commands = [
+            'conf t',
+            'interface ethernet 1/1',
+            'description configDeneme',
+            'no shutdown'
+        ]
+        output4 = net_connect.send_config_set(config_commands)
+        print("\nKonfigürasyon değişikliği sonucu:\n")
+        output5 = net_connect.send_command('show run interface ethernet 1/1')
+        print(output4)
+        print(output5)
 
-output3 = net_connect.send_command('show mac address-table')
-print("\nshow mac address komutunun sonucu:\n")
-print(output3)
+        # Yedekleme komutunu gönder
+        output = net_connect.send_command('write memory')
+        print("Yedekleme komutunun sonucu:\n")
+        print(output)
 
-# Örnek 3: Cihazın konfigürasyonunu değiştirme
-config_commands = [
-    'conf t',
-    'interface ethernet 1/1',
-    'description configDeneme',
-    'no shutdown'
-]
-output4 = net_connect.send_config_set(config_commands)
-print("\nKonfigürasyon değişikliği sonucu:\n")
-output5 = net_connect.send_command('show run interface ethernet 1/1')
-print(output4)
-print(output5)
+        # Bağlantıyı kapat
+        net_connect.disconnect()
 
-# Yedekleme komutunu gönder
-output = net_connect.send_command('write memory')
-print("Yedekleme komutunun sonucu:\n")
-print(output)
+    except NetMikoTimeoutException:
+        print("Bağlantı zaman aşımına uğradı. Cihaz erişimi mümkün olmayabilir.")
+    except NetMikoAuthenticationException:
+        print("Kimlik doğrulama hatası. Kullanıcı adı veya şifre hatalı olabilir.")
+    except Exception as e:
+        print(f"Beklenmeyen bir hata oluştu: {str(e)}")
 
-# Bağlantıyı kapat
-net_connect.disconnect()
+# Kullanıcıya hangi switch için işlem yapmak istediğini sormak
+while True:
+    switch_choice = input("Hangi switch için işlem yapmak istiyorsunuz? (Q/q to exit, 1 for 192.168.1.103, 2 for 192.168.1.104): ").lower()
+    if switch_choice == 'q':
+        break
+    elif switch_choice == '1':
+        connect_to_switch(ip_address='192.168.1.103')
+    elif switch_choice == '2':
+        connect_to_switch(ip_address='192.168.1.104')
+    else:
+        print("Geçersiz giriş. Lütfen doğru bir switch numarası girin.")
